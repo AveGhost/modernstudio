75b93b6b5ccbd1bf464394a8d1c33485c1053800
aveghost <aveghost359@gmail.com>
2025-05-15 11:32:12 +0200

    Some css changes

--- src/app/components/highlight-heading/highlight-heading.component.tsx
+++ src/app/components/highlight-heading/highlight-heading.component.tsx
@@ -3,12 +3,13 @@ interface Props {
     fontSize?: string
     isRight?: boolean
     classes?: string
+    highLightWidth?: string
 }
 
-const HighlightHeading = ({children, fontSize, isRight, classes}: Props) => {
+const HighlightHeading = ({children, fontSize, isRight, classes, highLightWidth}: Props) => {
     return (
         <h2 className={`w-fit font-medium relative ${classes ? classes : ''}`} style={{fontSize: fontSize}}>{children}
-          <span className={`absolute ${isRight ? 'right-0' : 'left-0'} top-0 bottom-0 w-full max-w-[280px] -z-10 bg-[#DDD4C9]`} style={{ height: `calc(${fontSize} * 1.5)` }} />
+          <span className={`absolute ${isRight ? '-right-2' : '-left-2'} top-0 bottom-0 w-full -z-10 bg-[#DDD4C9]`} style={{ height: `calc(${fontSize} * 1.5)`, maxWidth: highLightWidth }} />
         </h2>
     )
 }
