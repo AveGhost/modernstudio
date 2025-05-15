6f0f6976468650e4a3f4907432381b7bf5268d56
aveghost <aveghost359@gmail.com>
2025-05-15 19:25:44 +0200

    Added home url to logo

--- src/app/components/navigation/navigation.component.tsx
+++ src/app/components/navigation/navigation.component.tsx
@@ -1,10 +1,17 @@
+'use client'
+
 import Image from "next/image"
 import NavigationListWrapper from "./navigation-list/navigation-list-wrapper.component"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const Navigation = () => {
+  const pathname = usePathname();
+  const home = pathname === "/"
     return (
       <nav className="flex justify-between items-center relative p-4">
-        <Image src="/Logo.png" alt="Logo" width={96} height={96} className="z-40" />
+        {home ? <Image src="/Logo.png" alt="Logo" width={96} height={96} className="z-40" />
+        : <Link href="/"><Image src="/Logo.png" alt="Logo" width={96} height={96} className="z-40" /></Link>}
         <NavigationListWrapper />
       </nav>
     )
