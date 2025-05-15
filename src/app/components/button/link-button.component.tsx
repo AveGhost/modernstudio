0042df1250707cba0430e80aa32ba78bdd350f8b
aveghost <aveghost359@gmail.com>
2025-05-15 12:03:22 +0200

    Added hover animation

--- src/app/components/button/link-button.component.tsx
+++ src/app/components/button/link-button.component.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const LinkButton = ({children, href}: Props) => {
     return (
-        <Link href={href} className="border-[#444C54] border-1 p-2 uppercase font-medium text-[12px]">{children}</Link>
+        <Link href={href} className="border-[#444C54] border-1 p-2 overflow-hidden uppercase font-medium text-[12px] relative hover:text-white transition-colors duration-300 before:content-[''] before:absolute before:left-1/2 before:top-1/2 before:w-[300px] before:h-[300px] before:translate-x-[-50%] before:scale-0 before:translate-y-[-50%] before:rounded-full before:bg-[#444C54] before:transition-all before:duration-500 before:z-[-1] hover:before:scale-100 hover:before:translate-x-[-50%] hover:before:translate-y-[-50%]">{children}</Link>
     )
 }
 
