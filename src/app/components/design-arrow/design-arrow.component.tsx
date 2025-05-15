b8b479e8d4d2f0ee33cfc49d7b09d72a056b29d0
aveghost <aveghost359@gmail.com>
2025-05-15 16:06:31 +0200

    Adjust to mobile devices

--- src/app/components/design-arrow/design-arrow.component.tsx
+++ src/app/components/design-arrow/design-arrow.component.tsx
@@ -1,19 +1,21 @@
-const DesignArrow = ({direction, borderColor} : {direction: string, borderColor: string}) => {
+
+
+const DesignArrow = ({direction, borderColor, classes} : {direction: string, borderColor: string, classes?: string}) => {
     const desiginArrowDirection = (direction: string) => {
         if (direction === 'top-right') {
-            return '-top-10 -right-10 border-t-25 border-r-25'
+            return 'md:-top-7 md:-right-7 md:border-t-15 md:border-r-15 lg:-top-10 lg:-right-10 lg:border-t-25 lg:border-r-25 -top-5 -right-5 border-t-10 border-r-10'
         } else if (direction === 'top-left') {
-            return '-top-10 -left-10 border-t-25 border-l-25'
+            return 'md:-top-7 md:-left-7 md:border-t-15 md:border-l-15 lg:-top-10 lg:-left-10 lg:border-t-25 lg:border-l-25 -top-5 -left-5 border-t-10 border-l-10'
         } else if (direction === 'bottom-right') {
-            return '-bottom-10 -right-10 border-b-25 border-r-25'
+            return 'md:-bottom-7 md:-right-7 md:border-b-15 md:border-r-15 lg:-bottom-10 lg:-right-10 lg:border-b-25 lg:border-r-25 -bottom-5 -right-5 border-b-10 border-r-10'
         } else if (direction === 'bottom-left') {
-            return '-bottom-10 -left-10 border-b-25 border-l-25'
+            return 'md:-bottom-7 md:-left-7 md:border-b-15 md:border-l-15 lg:-bottom-10 lg:-left-10 lg:border-b-25 lg:border-l-25 -bottom-5 -left-5 border-b-10 border-l-10'
         }
     }
 
     return (
         <span 
-        className={`bg-transparent absolute ${desiginArrowDirection(direction)} block w-[158px] h-[158px]`} 
+        className={`bg-transparent absolute ${desiginArrowDirection(direction)} block w-17 h-17 md:w-[158px] md:h-[158px] ${classes ? classes : ''}` } 
         style={{borderColor: borderColor}}/>
     )
 }
