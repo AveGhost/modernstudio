7fee601b8a7e859197f6194e044501dd8bf1d010
aveghost <aveghost359@gmail.com>
2025-05-15 16:07:23 +0200

    Added hover

--- src/app/components/link-with-icon/link-with-icon.component.tsx
+++ src/app/components/link-with-icon/link-with-icon.component.tsx
@@ -1,9 +1,12 @@
 import { Icon } from "@iconify/react/dist/iconify.js"
 import Link from "next/link"
 
-const LinkWithIcon = ({children, href}: {children: React.ReactNode, href: string}) => {
+const LinkWithIcon = ({children, href, classes}: {children: React.ReactNode, href: string, classes?: string}) => {
     return (
-        <Link href={href} className="flex items-center gap-2 underline w-fit">{children} <Icon icon="mynaui:arrow-right" width="24" height="24" /></Link>
+        <Link href={href} className={`flex items-center gap-2 underline w-fit group hover:text-[#222]/70 transition-colors duration-300 ${classes ? classes : ''}`}>
+            {children} 
+            <Icon icon="mynaui:arrow-right" className="group-hover:ml-2 text-[#222] transition-all duration-300" width="24" height="24" />
+        </Link>
     )
 }
 
