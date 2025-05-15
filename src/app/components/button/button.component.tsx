0fc25ba10f61a641a7889e9acfb73963c25e3cf8
aveghost <aveghost359@gmail.com>
2025-05-15 11:31:42 +0200

    Some css changes

--- src/app/components/button/button.component.tsx
+++ src/app/components/button/button.component.tsx
@@ -1,12 +1,13 @@
 interface Props {
-    text: string
+    children: React.ReactNode
     type?: "button" | "submit"
     onClick?: () => void
+    classes?: string
 }
 
-const Button = ({text, type, onClick}: Props) => {
+const Button = ({children, type, onClick, classes}: Props) => {
     return (
-        <button type={!type ? 'button' : type} onClick={onClick} className="border-[#444C54] p-2 uppercase">{text}</button>
+        <button type={!type ? 'button' : type} onClick={onClick} className={`border-[#444C54] w-fit border-1 p-2 uppercase ${classes ? classes : ''}`}>{children}</button>
     )
 }
 
