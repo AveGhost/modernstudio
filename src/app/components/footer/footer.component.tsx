d45c7b5bd19c6ae3c928d6d47054c20e382a60cf
aveghost <aveghost359@gmail.com>
2025-05-15 16:07:14 +0200

    Adjust to mobile devices

--- src/app/components/footer/footer.component.tsx
+++ src/app/components/footer/footer.component.tsx
@@ -1,19 +1,18 @@
-import Link from "next/link"
 import Image from "next/image"
 import NavigationListElement from "../navigation/navigation-list/navigation-list-element.component"
 
 const Footer = () => {
     return (
-        <footer className="pb-14">
-            <p className="!font-[family-name:var(--font-inter)] font-bold mb-8">Modren studio</p>
-            <ul className="grid grid-cols-3 gap-2">
+        <footer className="pb-14 px-5 grid grid-cols-2 gap-6 md:block md:gap-0 items-center">
+            <p className="!font-[family-name:var(--font-inter)] font-bold md:mb-8">Modren studio</p>
+            <ul className="grid grid-cols-2 md:grid-cols-3 gap-2 col-span-2">
                 <NavigationListElement href="/" name="Oferty" />
                 <NavigationListElement href="/" name="Zespół" />
                 <NavigationListElement href="/" name="Kontakt" />
                 <NavigationListElement href="/" name="Realizacje" />
                 <NavigationListElement href="/" name="Usługi" />
             </ul>
-            <div className="flex items-center justify-end -mr-15">
+            <div className="flex items-center ml-auto col-2 row-1 md:ml-0 md:justify-end lg:-mr-15">
                 <Image src="/icons/social/facebook.svg" alt="Facebook" width={32} height={32} className="mx-4" />
                 <Image src="/icons/social/instagram.svg" alt="Instagram" width={32} height={32} className="mx-4" />
             </div>
--- src/app/components/navigation/navigation-list/navigation-list-element.component.tsx
+++ src/app/components/navigation/navigation-list/navigation-list-element.component.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const NavigationListElement = ({href, name, classes}: {href: string, name: string, classes?: string}) => {
     return (
-        <li><Link href={href} className={`relative w-fit overflow-hidden text-[15px] before:content-[''] before:w-[50px] before:h-[1px] before:absolute before:bg-[#444C54] before:left-0 before:right-0 before:bottom-0 before:mx-auto before:scale-x-0 before:transition-all before:duration-300 hover:before:-scale-x-100 ${classes ? classes : ''}`}>{name}</Link></li>
+        <li><Link href={href} className={`relative w-fit overflow-hidden text-[15px] before:content-[''] before:w-[50px] before:h-[1px] before:absolute before:bg-[#444C54] before:left-0 before:right-0 before:-bottom-1 before:mx-auto before:scale-x-0 before:transition-all before:duration-300 hover:before:-scale-x-100 ${classes ? classes : ''}`}>{name}</Link></li>
     )
 }
 
