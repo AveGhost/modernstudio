25eac0bd766091bf02a62c01500288d8303fbe70
aveghost <aveghost359@gmail.com>
2025-05-23 16:01:28 +0200

    Added link to offers

--- src/app/oferty/page.tsx
+++ src/app/oferty/page.tsx
@@ -8,53 +8,55 @@ const offers = [
         name: "Apartament z tarasem na dachu",
         location: "Rolna 48",
         image: "/offers/oferta_1.jpg",
-        href: "/"
+        href: "/oferty/Apartament z tarasem na dachu"
     },
     {
         id: 2,
         name: "Nowoczesny loft w sercu poznania",
         location: "Bóżnicza 1",
         image: "/offers/oferta_2.jpg",
-        href: "/"
+        href: "/oferty/Nowoczesny loft w sercu poznania"
     },
     {
         id: 3,
         name: "Przytulne mieszkanie z duszą",
         location: "Garbary 30",
         image: "/offers/oferta_3.jpg",
-        href: "/"
+        href: "/oferty/Przytulne mieszkanie z duszą"
     },
         {
         id: 4,
         name: "Magiczna przestrzeń dla dużej rodziny",
         location: "Jeleniogórska 5",
         image: "/offers/oferta_4.jpg",
-        href: "/"
+        href: "/oferty/Magiczna przestrzeń dla dużej rodziny"
     }
 ]
 
 const Offers = () => {
     return (
         <main className="px-6 xl:px-0">
-            <div className="mx-auto flex flex-col lg:justify-center lg:items-center lg:text-center gap-6">
-                <HighlightHeading fontSize="50px" highLightWidth="150px">Nasza oferta</HighlightHeading>
-                <p>Nieustannie poszukujemy nowych ofert nieruchomości. Wybierz<br/> typ nieruchomości, który Cię interesuje.</p>
+            <div className="max-w-[1270px] mx-auto">
+                <div className="mx-auto flex flex-col lg:justify-center lg:items-center lg:text-center gap-6">
+                    <HighlightHeading fontSize="50px" highLightWidth="150px">Nasza oferta</HighlightHeading>
+                    <p>Nieustannie poszukujemy nowych ofert nieruchomości. Wybierz<br/> typ nieruchomości, który Cię interesuje.</p>
+                </div>
+                <section className="pb-10 md:pb-40">
+                    <div className="flex items-center justify-between lg:justify-center lg:gap-35 py-11">
+                        <button className="font-bold">Mieszkanie</button>
+                        <button className="font-bold">Dom</button>
+                        <button className="font-bold">Działka</button>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 relative">
+                        <DesignArrow direction="top-left" borderColor="#444C54" classes="hidden xl:block"/>
+                        {offers.map((offer,index) => {
+                            return (
+                                <OfferCard key={index} location={offer.location} title={offer.name} href={offer.href} image={offer.image}/>
+                            )
+                        })}
+                    </div>
+                </section>
             </div>
-            <section className="pb-10 md:pb-40">
-                <div className="flex items-center justify-between lg:justify-center lg:gap-35 py-11">
-                    <button className="font-bold">Mieszkanie</button>
-                    <button className="font-bold">Dom</button>
-                    <button className="font-bold">Działka</button>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 relative">
-                    <DesignArrow direction="top-left" borderColor="#444C54" classes="hidden xl:block"/>
-                    {offers.map((offer,index) => {
-                        return (
-                            <OfferCard key={index} location={offer.location} title={offer.name} href={offer.href} image={offer.image}/>
-                        )
-                    })}
-                </div>
-            </section>
         </main>
     )
 }
