81289483cbdf5ce61d04c6b7045f40b58eec33c3
aveghost <aveghost359@gmail.com>
2025-05-29 15:42:51 +0200

    Fixed typo and change icons from image to iconify

---
+++ src/app/components/socials/social.component.tsx
@@ -0,0 +1,37 @@
+import { Icon } from "@iconify/react/dist/iconify.js"
+const SocialIcons = ({classes}: {classes?: string}) => {
+    return (
+        <div className={`flex ${classes ? classes : ''}`}>
+            <Icon icon="ic:twotone-facebook" width="24" height="24" className="mx-4 w-12 h-12 md:w-8 md:h-8 hover:text-[#3B5998] transition-colors duration-300 cursor-pointer"/>
+            <span className="mx-4 w-12 h-12 md:w-8 md:h-8 cursor-pointer relative inline-block">
+                <svg
+                className=""
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                >
+                    <defs>
+                        <linearGradient id="ig-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                        <stop offset="0%" stopColor="#f09433" />
+                        <stop offset="25%" stopColor="#e6683c" />
+                        <stop offset="50%" stopColor="#dc2743" />
+                        <stop offset="75%" stopColor="#cc2366" />
+                        <stop offset="100%" stopColor="#bc1888" />
+                        </linearGradient>
+                    </defs>
+                    <g className="icon-base">
+                        <path fill="currentColor" d="M13.61 12.243a1.6 1.6 0 1 1-1.56-1.63a1.62 1.62 0 0 1 1.56 1.63"/>
+                        <path fill="currentColor" d="M14.763 7.233H9.338a2.024 2.024 0 0 0-2.024 2.024v5.547a2.024 2.024 0 0 0 2.024 2.024h5.425a2.024 2.024 0 0 0 2.024-2.024V9.267a2.026 2.026 0 0 0-2.024-2.034m-2.713 7.723a2.703 2.703 0 1 1 2.642-2.703a2.67 2.67 0 0 1-2.642 2.703m2.936-5.405a.496.496 0 0 1-.496-.506a.506.506 0 1 1 1.012 0a.496.496 0 0 1-.557.506z"/>
+                        <path fill="currentColor" d="M12.05 2a10 10 0 1 0-.1 20a10 10 0 0 0 .1-20m6.073 12.702a3.39 3.39 0 0 1-3.41 3.411H9.389a3.39 3.39 0 0 1-3.411-3.41V9.378a3.39 3.39 0 0 1 3.41-3.411h5.325a3.39 3.39 0 0 1 3.41 3.41z"/>
+                    </g>
+                    <g className="icon-gradient">
+                        <path fill="url(#ig-gradient)" d="M13.61 12.243a1.6 1.6 0 1 1-1.56-1.63a1.62 1.62 0 0 1 1.56 1.63"/>
+                        <path fill="url(#ig-gradient)" d="M14.763 7.233H9.338a2.024 2.024 0 0 0-2.024 2.024v5.547a2.024 2.024 0 0 0 2.024 2.024h5.425a2.024 2.024 0 0 0 2.024-2.024V9.267a2.026 2.026 0 0 0-2.024-2.034m-2.713 7.723a2.703 2.703 0 1 1 2.642-2.703a2.67 2.67 0 0 1-2.642 2.703m2.936-5.405a.496.496 0 0 1-.496-.506a.506.506 0 1 1 1.012 0a.496.496 0 0 1-.557.506z"/>
+                        <path fill="url(#ig-gradient)" d="M12.05 2a10 10 0 1 0-.1 20a10 10 0 0 0 .1-20m6.073 12.702a3.39 3.39 0 0 1-3.41 3.411H9.389a3.39 3.39 0 0 1-3.411-3.41V9.378a3.39 3.39 0 0 1 3.41-3.411h5.325a3.39 3.39 0 0 1 3.41 3.41z"/>
+                    </g>
+                </svg>
+            </span>
+        </div>
+    )
+}
+
+export default SocialIcons
