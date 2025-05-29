855486bd24d8767e77c6afe7b4d075b4d528e89e
aveghost <aveghost359@gmail.com>
2025-05-29 15:42:20 +0200

    Now links are globaly exported insted of be only in navigation

---
+++ src/data/links.ts
@@ -0,0 +1,22 @@
+export const Links = [
+    {
+        name: "Oferty",
+        href: "/oferty"
+    },
+    {
+        name: "Realizacje",
+        href: "/realizacje"
+    },
+    {
+        name: "Zespoł",
+        href: "/zespol"
+    },
+    {
+        name: "Usługi",
+        href: "/uslugi"
+    },
+    {
+        name: "Kontakt",
+        href: "/kontakt"
+    }
+]
